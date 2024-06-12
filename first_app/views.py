409967e755abdc6d8147cf06be2bd1bd00905fc2
selcuk <yilmax4@gmail.com>
2024-06-12 22:11:42 +0300

    api_view decorator created and tested infirst_app folder, views.py file

--- first_app/views.py
+++ first_app/views.py
@@ -4,42 +4,45 @@ from rest_framework.parsers import JSONParser
 from .models import Country
 from .serializers import CountrySerializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
 
 
-@csrf_exempt
+@api_view(["GET","POST"])
 def country_list(request):
     if request.method == "GET":
         obj = Country.objects.all()
         serializer = CountrySerializer (obj,many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = CountrySerializer(data=data)
+        # data = JSONParser().parse(request)api_view
+        serializer = CountrySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-    return HttpResponse(status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["GET","PUT","DELETE"])
 def country_detail(request,pk):
     try:
         country = Country.objects.get(pk=pk)
     except Country.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
     if request.method == "GET":
         serializer = CountrySerializer(country)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = CountrySerializer(country,data=data)
+        # data = JSONParser().parse(request)
+        serializer = CountrySerializer(country,data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors,  status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
         country.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
