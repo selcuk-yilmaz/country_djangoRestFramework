a01b90fc637b8b55e3faaeafdbbe385b2d12f279
selcuk <yilmax4@gmail.com>
2024-06-12 14:32:41 +0300

    Function based functions were written in the view file of the first app. Additionally, it was tested that CRUD operations were working.

---
+++ first_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import country_list, country_detail
+
+urlpatterns = [
+    path("list/", country_list),
+    path("detail/<int:pk>", country_detail),
+
+
+]
