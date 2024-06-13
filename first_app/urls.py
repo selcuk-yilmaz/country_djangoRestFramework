a61496f94e037cd54a816ae1956495a2491e56df
selcuk <yilmax4@gmail.com>
2024-06-13 10:30:47 +0300

    class based api view created and tested in first app

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from .views import country_list, country_detail
+# from .views import country_list, country_detail #Function based usage
+from .views import CountryView, CountryDetailView
+
+
 
 urlpatterns = [
-    path("list/", country_list),
-    path("detail/<int:pk>", country_detail),
+    # path("list/", country_list),              #Function based usage
+    # path("detail/<int:pk>", country_detail),  #Function based usage
+    path("list/", CountryView.as_view()),       #Class based usage
+    path("detail/<int:pk>", CountryDetailView.as_view()),   #Class based usage
 
 
 ]
